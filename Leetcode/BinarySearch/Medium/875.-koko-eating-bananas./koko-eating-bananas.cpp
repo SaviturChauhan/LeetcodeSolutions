5ffa30f7d7ef73f52ddde08b6d898a9fdf7fedf5
SaviturChauhan <chauhansavitur@gmail.com>
2026-06-17 09:25:55 +0530

    Add solution: Solve "875. Koko Eating Bananas"

---
+++ Leetcode/BinarySearch/Medium/875.-koko-eating-bananas./koko-eating-bananas.cpp
@@ -0,0 +1,14 @@
+}
+        return time <= h;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int l = 1;
+        int r = *max_element(piles.begin(),piles.end());
+        }
+                if(piles[i] % mid != 0) time++;
+                time += piles[i]/mid;
+        for(int i = 0;i<piles.size();i++){
+        int time = 0;
+    bool solve(vector<int>& piles, int h,int mid){
+public:
+class Solution {
