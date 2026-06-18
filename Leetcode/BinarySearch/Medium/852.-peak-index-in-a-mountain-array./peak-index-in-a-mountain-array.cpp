3a8a00e5b9f2ea15baa15304bfc7cf63425c61b3
SaviturChauhan <chauhansavitur@gmail.com>
2026-06-18 12:33:35 +0530

    Add solution: Solve "852. Peak Index in a Mountain Array"

---
+++ Leetcode/BinarySearch/Medium/852.-peak-index-in-a-mountain-array./peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,19 @@
+if(arr[mid] > arr[mid-1]){
+               pivot = mid;
+               l = mid+1;
+            int mid = l + (r-l)/2;
+        while(l <= r){
+            }else{
+                r = mid-1;
+        int r = n-1;
+        int l = 1;
+        int pivot = -1;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+public:
+class Solution {
+            }
+        }
+        return pivot;
+    }
+};
